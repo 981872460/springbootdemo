718a04341ba78d406de8c11f31b499608de0178f
981872460 <jiguang981/*/>
2019-10-29 09:25:14 +0800

    修改

--- src/main/java/com/zql/biz/impl/TestServiceImpl.java
+++ src/main/java/com/zql/biz/impl/TestServiceImpl.java
@@ -3,28 +3,34 @@ package com.zql.biz.impl;
 import com.zql.biz.TestService;
 import com.zql.common.dao.ScoreMapper;
 import com.zql.common.dto.Score;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class TestServiceImpl implements TestService {
+    private static final Logger logger = LoggerFactory.getLogger(TestServiceImpl.class);
+
     private static String NUM = null;
 
     @Resource
     private ScoreMapper scoreMapper;
 
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public List<Score> scoreList() {
-        int a = 0;
-        System.out.println();
-        for (int i = 0; i < 1000; i++) {
-            a++;
-//            System.out.println(a);
-        }
-        System.out.println(a);
+        Score s = new Score();
+        s.setId(8);
+        s.setClas("36");
+        s.setScores(13);
+        s.setUserId(32);
+        scoreMapper.updateByPrimaryKey(s);
+        scoreMapper.insert(s);
         return scoreMapper.scoreList();
     }
 
